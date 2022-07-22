d9270d21a96721e5b539c0707f4ff6d4c3afa585
kevinzamperetti <kevin.zamperetti92@gmail.com>
2022-07-21 22:22:10 -0300

    Mentoria e Curso

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso de Java");
+        curso1.setDescricao("Descrição Curso de Java");
+        curso1.setCargaHoraria(8);
+        System.out.println(curso1);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso de JavaScript");
+        curso2.setDescricao("Descrição Curso de Java");
+        curso2.setCargaHoraria(4);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição da Mentoria de Java");
+        mentoria.setData(LocalDate.now());
+        System.out.println(mentoria);
+    }
+}
